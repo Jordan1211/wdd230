8c6030d38c64b5cbbde19f62b198f87831e5cecf
Jordan1211 <gre20057@byui.edu>
2021-10-17 08:55:48 -0600

    js and nav

--- lesson4/js/toggleMenu.js
+++ lesson4/js/toggleMenu.js
@@ -1,4 +1,8 @@
-function toggleMenu() {
-    console.log(document.getElementById("primaryNav").classList);
-    document.getElementById("primaryNav").classList.toggle("hide");
-}
+const options = {weekday: 'long'};
+document.getElementById('today').textContent = new Date().toLocaleDateString(options);
+
+
+// function toggleMenu() {
+   // console.log(document.getElementById("primaryNav").classList);
+   // document.getElementById("primaryNav").classList.toggle("hide");
+//}
