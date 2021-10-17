2f90a14ce6110c81211d6bee73ec2cefd1757cf8
Jordan1211 <gre20057@byui.edu>
2021-10-17 11:42:45 -0600

    fixed friday message js

--- lesson4/js/toggleMenu.js
+++ lesson4/js/toggleMenu.js
@@ -1,8 +1,4 @@
-const options = {weekday: 'long'};
-document.getElementById('today').textContent = new Date().toLocaleDateString(options);
-
-
-// function toggleMenu() {
-   // console.log(document.getElementById("primaryNav").classList);
-   // document.getElementById("primaryNav").classList.toggle("hide");
-//}
+function toggleMenu() {
+    console.log(document.getElementById("primaryNav").classList);
+    document.getElementById("primaryNav").classList.toggle("hide");
+}
