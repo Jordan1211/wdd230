507f2f2d2a281c0ca4e7d9b805cd4f29f0fd8672
Jordan1211 <gre20057@byui.edu>
2021-10-27 17:03:58 -0600

    updates & week 7

--- lesson6/js/windChill.js
+++ lesson6/js/windChill.js
@@ -2,16 +2,19 @@
 // output: Wind Chill
 // processing: calculate wind chill by (f = 35.74 + 0.6215 t - 35.75 s0.16 + 0.4275 t s0.16)
 
-function doInputOutput() {
-    let temp = document.getElementById('temp').value;
-    let speed = document.getElementById('speed').value;
-    let chill = parseFloat(windChill(temp, speed));
+// function doInputOutput() {
+    let temp = parseFloat(document.querySelector('#temp').textContent);
+    let speed = parseFloat(document.querySelector('#speed').textContent);
+//    let chill = parseFloat(windChill(temp, speed));
     
-    document.getElementById('output').innerHTML = "Wind Chill is: " + chill.toFixed(2) + "\u00B0F";
-}
+    // document.getElementById('output').innerHTML = "Wind Chill is: " + chill.toFixed(2) + "\u00B0F";
+// }
 
-function windChill(tempF, speed) {
-    windChill = 35.74 + 0.6215 * tempF - 35.75 * speed ** 0.16 + 0.4275 * tempF * speed ** 0.16
-}
-
-document.getElementById('windChill').textContent = windChill;
+// function windChill(tempF, speed) {
+    if (temp >= 50 && speed > 3 ) {
+        let result = 35.74 + 0.6215 * temp - 35.75 * speed ** 0.16 + 0.4275 * temp * speed ** 0.16
+    }
+    else {
+        let result = 'N/A'
+        
+document.querySelector('#windChill').textContent = result.toFixed(1); }
