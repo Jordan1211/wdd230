314de5a2d9ffa588a11f4e952b97e855bb4d9dd1
Jordan1211 <gre20057@byui.edu>
2021-11-04 23:23:10 -0600

    updates to 6

---
+++ lesson9/js/api.js
@@ -0,0 +1,27 @@
+fetch("https://maps.googleapis.com/maps/api/js?key=AIzaSyChcfH2ffkBU-mqtryWx_zBXhNsWy-Ycgk");
+
+"https://www.nap.st/google_map_generator/?lang=en"
+
+google.maps.event.addDomListener(window, 'load', function() {
+	var mapdiv = document.getElementById('user_gmap');
+	var myOptions = {
+	zoom: 3,
+	center: new google.maps.LatLng(43.056709804020926,-84.92234630041509),
+	mapTypeId: google.maps.MapTypeId.ROADMAP,
+	scaleControl: true
+	};
+	var map = new google.maps.Map(mapdiv, myOptions);
+	var marker = new google.maps.Marker({
+	position: new google.maps.LatLng(43.056709804020926,-84.92234630041509),
+	map: map, 
+	title: ''
+	});
+	var infowindow = new google.maps.InfoWindow({
+	content: '<strong></strong><br />',
+	size: new google.maps.Size(200, 200)
+	});
+	google.maps.event.addListener(marker, 'click', function() {
+	infowindow.open(map,marker);
+	});
+	
+});
